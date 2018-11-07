92ab580666a4f7c70e1e433070da2b3aab536509
Michał Dolny <michal.dolny@gmail.com>
2018-11-07 14:24:35 +0100

    boilerplate - react, babel, webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = (env) => {
+  const environment = env || 'production';
+
+  return {
+    mode: environment,
+    entry: './src/index.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public')
+    }
+  }
+};
