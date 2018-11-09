21f03f0c18f363e9dcc3c29883297a0b8382bf40
Michał Dolny <michal.dolny@gmail.com>
2018-11-09 02:09:06 +0100

    handle change number in inputs

--- src/components/CrossMultiplicationForm.js
+++ src/components/CrossMultiplicationForm.js
@@ -2,14 +2,31 @@ import React from 'react';
 
 export default class CrossMultiplicationForm extends React.Component {
   state = {
-    numbers: [1, 0, 0, 0]
+    numbers: [1, 2, 3, 4]
   };
   handleChangeNumber = (e) => {
-    const newNumber = Number(e.target.value);
+    const inputName = e.target.name;
+    let inputIndex = undefined;
+    switch (inputName) {
+      case 'topLeft':
+        inputIndex = 0;
+        break;
+      case 'topRight':
+        inputIndex = 1;
+        break;
+      case 'bottomLeft':
+        inputIndex = 2;
+        break;
+      case 'bottomRight':
+        inputIndex = 3;
+        break;
+    }
+    const newNumber = (e.target.validity.valid) ? e.target.value : this.state.numbers[inputIndex];
+
     this.setState(prevState => {
       const numbers = this.state.numbers.map((number, i) => {
-        if (i === 0) {
-          return newNumber;
+        if (i === inputIndex) {
+          return Number(newNumber);
         }
         return number;
       });
@@ -25,12 +42,40 @@ export default class CrossMultiplicationForm extends React.Component {
     return (
       <form onSubmit={this.handleCrossMultiplication}>
         <div>
-          <input type="number" name="topLeft" value={this.state.numbers[0]} onChange={this.handleChangeNumber} />
-          <input type="number" name="topRight" value={this.state.numbers[2]} />
+          <input
+            type="text"
+            pattern="[0-9]*"
+            name="topLeft"
+            data-lpignore="true"
+            value={this.state.numbers[0]}
+            onChange={this.handleChangeNumber}
+          />
+          <input
+            type="text"
+            pattern="[0-9]*"
+            name="topRight"
+            data-lpignore="true"
+            value={this.state.numbers[1]}
+            onChange={this.handleChangeNumber}
+          />
         </div>
         <div>
-          <input type="number" name="bottomLeft" value={this.state.numbers[1]} />
-          <input type="number" name="bottomRight" value={this.state.numbers[3]} />
+          <input
+            type="text"
+            pattern="[0-9]*"
+            name="bottomLeft"
+            data-lpignore="true"
+            value={this.state.numbers[2]}
+            onChange={this.handleChangeNumber}
+          />
+          <input
+            type="text"
+            pattern="[0-9]*"
+            name="bottomRight"
+            data-lpignore="true"
+            value={this.state.numbers[3]}
+            onChange={this.handleChangeNumber}
+          />
         </div>
         <button>Calculate</button>
       </form>
